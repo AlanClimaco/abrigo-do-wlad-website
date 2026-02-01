337466b46c09e61810d6a482d6aa9c369215b70a
Luis Gustavo R.C. <59234999+spantalho@users.noreply.github.com>
2026-02-01 13:50:50 -0300

    adds cache

--- api/get-daily-dog.ts
+++ api/get-daily-dog.ts
@@ -8,6 +8,11 @@ export default async function handler(
   try {
     const dog = await kv.get("daily-dog");
 
+    // 23h cache
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=82800, stale-while-revalidate=3600",
+    );
     res.setHeader("Content-Type", "application/json");
 
     if (dog) {
