b83fd0646cec55cc222ebd134eec396074768e6e
Luis Gustavo R.C. <59234999+spantalho@users.noreply.github.com>
2026-04-28 21:32:05 -0300

    feat(acessibility): adds the VLibrasWidget component and plugin for accessibility

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { VLibrasWidget } from "./components/common/VLibrasWidget";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Recycle from "./pages/Recycle";
@@ -12,6 +13,7 @@ export function AppRoutes() {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <VLibrasWidget />
 
       <Header />
 
