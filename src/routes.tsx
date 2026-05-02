0d34a4f6e49187bed44867862b405fe475cd8044
Luis Gustavo R.C. <59234999+spantalho@users.noreply.github.com>
2026-05-02 19:25:22 -0300

    fix(routes): remove unused Form route and adjust navigation for the form

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Recycle from "./pages/Recycle";
 import Adopt from "./pages/Adopt";
-import Form from "./pages/Form";
 import PrivacyPolicy from "./pages/Legal";
 import ScrollToTop from "./components/common/ScrollToTop";
 import BetaForm from "./pages/BetaForm";
@@ -25,11 +24,14 @@ export function AppRoutes() {
         <Route path="/tampinhas" element={<Recycle />} />
         <Route path="/adotar" element={<Adopt />} />
         <Route
+          path="/formulario"
+          element={<Navigate to="/beta/formulario" />}
+        />
+        <Route
           path="/beta/formulario"
           element={<Navigate to="/beta/formulario/step/1" replace />}
         />
         <Route path="/beta/formulario/step/:step" element={<BetaForm />} />
-        <Route path="/formulario" element={<Form />} />
         <Route path="/politica-de-privacidade" element={<PrivacyPolicy />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
